edfb32cfa688ca038dcbc52fe8bd7bcf3e6bc425
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-05-31 14:20:28 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hotel</title>
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <div class="parallax">
+            <div id="group2" class="parallax__group">
+                <div class="parallax__layer parallax__layer--base">
+                    <div class="title">Base Layer</div>
+                </div>
+                <div class="parallax__layer parallax__layer--back">
+                    <div class="title">Background Layer</div>
+                </div>
+            </div>
+        </div>
+        <header class="mobile-menu">Navbar</header>
+        <section>Sugar plum muffin cookie pastry oat cake icing candy canes chocolate. Gummi bears chupa chups fruitcake dessert jelly. Muffin cookie ice cream soufflé pastry lollipop gingerbread sweet. Unerdwear.com bonbon candy marzipan bonbon gummies chocolate cakegummi bears powder. Unerdwear.com tart halvah chocolate cake dragée liquorice. Sugar plum chocolate bar pastry liquorice dragée jelly powder. Jelly tootsie roll applicake caramels. Marzipan candy tootsie roll donut. Gummies ice cream macaroon applicake.</section>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script>
+            $(window).scroll(function() {
+            var sticky = $('.mobile-menu'),
+                scroll = $(window).scrollTop();
+            
+            if (scroll >= 40) { 
+                sticky.addClass('fixed'); }
+            else { 
+            sticky.removeClass('fixed');
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-</head>
-
-<body class="antialiased">
-    @include('Template/head')
-
-    <!-- navbar -->
-    @include('Template/userNavbar')
-    <!-- end of navbar -->
-
-    @include('Template/script')
-</body>
-
+            }
+            });
+        </script>
+    </body>
 </html>
