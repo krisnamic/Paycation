ddf9d53630fe2fb6a427478448936f4d6edc1e33
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-05-31 15:07:50 +0700

    add home design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,33 +7,34 @@
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     </head>
     <body class="antialiased">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <div class="parallax">
             <div id="group2" class="parallax__group">
                 <div class="parallax__layer parallax__layer--base">
-                    <div class="title">Base Layer</div>
+                    <div class="title" style="font-size:3em; color: white">Hotel Murah Meriah</div>
                 </div>
                 <div class="parallax__layer parallax__layer--back">
-                    <div class="title">Background Layer</div>
+                    <div class="title"></div>
                 </div>
             </div>
         </div>
-        <header class="mobile-menu">Navbar</header>
-        <section>Sugar plum muffin cookie pastry oat cake icing candy canes chocolate. Gummi bears chupa chups fruitcake dessert jelly. Muffin cookie ice cream soufflé pastry lollipop gingerbread sweet. Unerdwear.com bonbon candy marzipan bonbon gummies chocolate cakegummi bears powder. Unerdwear.com tart halvah chocolate cake dragée liquorice. Sugar plum chocolate bar pastry liquorice dragée jelly powder. Jelly tootsie roll applicake caramels. Marzipan candy tootsie roll donut. Gummies ice cream macaroon applicake.</section>
+        <header class="menu">Navbar</header>
+        <section class="test" style="font-size: 6em;">Sugar plum muffin cookie pastry oat cake icing candy canes chocolate. Gummi bears chupa chups fruitcake dessert jelly. Muffin cookie ice cream soufflé pastry lollipop gingerbread sweet. Unerdwear.com bonbon candy marzipan bonbon gummies chocolate cakegummi bears powder. Unerdwear.com tart halvah chocolate cake dragée liquorice. Sugar plum chocolate bar pastry liquorice dragée jelly powder. Jelly tootsie roll applicake caramels. Marzipan candy tootsie roll donut. Gummies ice cream macaroon applicake.</section>
 
         <script>
             $(window).scroll(function() {
-            var sticky = $('.mobile-menu'),
-                scroll = $(window).scrollTop();
-            
-            if (scroll >= 40) { 
-                sticky.addClass('fixed'); }
-            else { 
-            sticky.removeClass('fixed');
-
-            }
+                var sticky = $('.menu'), scroll = $(window).scrollTop(); 
+                var text = $('.test');
+                
+                if (scroll >= 668) { 
+                    sticky.addClass('fixed'); 
+                    text.css('margin-top', '3.5rem');
+                } else { 
+                    sticky.removeClass('fixed');
+                    text.css('margin-top', '0');
+                }
             });
         </script>
     </body>
