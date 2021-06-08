3b720afcc641d3eea0a421073251674bd81b5238
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-06-08 23:58:40 +0700

    added shadow to home logo

--- resources/views/Template/head.blade.php
+++ resources/views/Template/head.blade.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>Paycation | Starter</title>
 
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{asset('AdminLTE/plugins/fontawesome-free/css/all.min.css')}}">
