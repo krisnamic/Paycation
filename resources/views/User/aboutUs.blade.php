f3eb4cd235e2707c1976d40b8670d9171f06b8e6
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-06-09 22:29:59 +0700

    improved detailHotel + aboutUs

--- resources/views/User/aboutUs.blade.php
+++ resources/views/User/aboutUs.blade.php
@@ -21,15 +21,15 @@
 
             <!-- Name -->
             <div>
-                <h2 class="aboutus-name">Steven Lie</h2>
+                <h2 class="aboutus-name"><b>Steven Lie</b></h2>
             </div>
 
             <!-- Desc -->
             <div>
                 <i class="aboutus-nim">000000XXXXX</i> <br><br>
-                Test <br>
-                Test <br>
-                Test <br>
+                Full-Stack Developer <br><br>
+                <i class="fab fa-github"></i> steven278 <br>
+                <i class="fab fa-instagram"></i> steven_lie9 <br>
             </div>
         </div>
 
@@ -37,7 +37,7 @@
         <div class="card d-flex flex-column justify-content-center text-center">
             <!-- Picture -->
             <div class="profile-frame d-flex flex-row justify-content-center">
-                <img src="{{asset('img/aboutUs/jerry.jpg')}}" class="profile-img" onmouseover="hover(this);" onmouseout="unhover(this);">
+                <img id="rotate" src="{{asset('img/aboutUs/jerry.jpg')}}" class="profile-img" onmouseover="hover(this);" onmouseout="unhover(this);">
             </div>
 
             <!-- Name -->
@@ -48,9 +48,9 @@
             <!-- Desc -->
             <div>
                 <i class="aboutus-nim">000000XXXXX</i> <br><br>
-                Test <br>
-                Test <br>
-                Test <br>
+                Full-Stack Developer <br><br>
+                <i class="fab fa-github"></i> jryarianto <br>
+                <i class="fab fa-instagram"></i> jryarianto_ <br>
             </div>
         </div>
 
@@ -69,9 +69,9 @@
             <!-- Desc -->
             <div>
                 <i class="aboutus-nim">000000XXXXX</i> <br><br>
-                Test <br>
-                Test <br>
-                Test <br>
+                Full-Stack Developer <br><br>
+                <i class="fab fa-github"></i> krisnamic <br>
+                <i class="fab fa-instagram"></i> krisna_mic <br>
             </div>
         </div>
 
@@ -90,14 +90,51 @@
             <!-- Desc -->
             <div>
                 <i class="aboutus-nim">000000XXXXX</i> <br><br>
-                Test <br>
-                Test <br>
-                Test <br>
+                Full-Stack Developer <br><br>
+                <i class="fab fa-github"></i> myqpalzm <br>
+                <i class="fab fa-instagram"></i> myqpalzm147 <br>
             </div>
         </div>
     </div>
 
+    @include('Template/footer')
+
     @include('Template/script')
+    <script>
+        function AnimateRotate(elt, d, duration, ondone){
+            $({deg: 0}).animate({deg: d}, {
+                easing: 'linear',
+                duration: duration,
+                step: function(now, fx){
+                    $(elt).css({
+                        transform: "rotate(" + now + "deg)"
+                    });
+                },
+                complete: ondone.bind(elt)
+            });
+        }
+
+        $.fn.loopingRotation = function(msPerRotation) {
+            if (this.data('rotating') === true) // already rotating
+                return;
+            this.data('rotating', true);
+            AnimateRotate(this, 360, msPerRotation, function() {
+                if (this.data('rotating') !== true)
+                    return;
+                this.data('rotating', false);
+                $(this).loopingRotation(msPerRotation);
+            });
+        }
+        $.fn.stopRotation = function() {
+            this.data('rotating', false);
+        }
+
+        $("#rotate").hover(function(){
+            $(this).loopingRotation(1000);
+        }, function(){
+            $(this).stopRotation();
+        });
+    </script>
 </body>
 
 </html>
