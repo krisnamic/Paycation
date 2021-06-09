5bec0e2dffa37d5d2cf27ed3d76a6cdf52f40c51
steven278 <darkavenger347@gmail.com>
2021-06-10 01:12:49 +0700

    booking history udah

--- resources/views/User/detailBooking.blade.php
+++ resources/views/User/detailBooking.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paycation | Booking Detail</title>
 </head>
+<style>
+    .media img {
+        height: 250px;
+    }
+</style>
 
 <body>
     @include('Template/head')
@@ -13,29 +18,31 @@
     <!-- navbar -->
     @include('template/userNavbar')
     <!-- end of navbar -->
-    <h1>detailbooking page</h1>
 
     <!-- content -->
     <div class="container">
-        @foreach($pesanan as $p)
-        <div class="card">
-            <div class="media position-relative ">
-                <img src="{{asset('img/hotel/'.$p->gambarHotel1)}}" class="mr-3 mt-4 ml-3 mb-3" alt="..." style="width:250px;">
-                <div class="media-body mt-0 mb-3">
-                    <h5>Pesanan 00000{{$pesanan2[$loop->index]->id}}</h5>
-                    <h5 class="mt-3 detail-text">{{$p->namaHotel}}</h5>
-                    <p style="margin-bottom:0.25rem;">Price : Rp. {{$p->hargaPesanan}} ,-</p>
-                    <p style="margin-bottom:0.25rem;">Check In : {{$p->checkIn}}</p>
-                    <p style="margin-bottom:0.25rem;">Check Out : {{$p->checkOut}}</p>
-                    <p style="margin-bottom:0.25rem;">Book Time : {{$p->jamBooking}}</p>
-                    <p style="margin-bottom:0.25rem;">Book Date : {{$p->tanggalBooking}}</p>
-                    <a href="/invoice/{{$pesanan2[$loop->index]->id}}" class="stretched-link">View Detail Booking</a>
+        <div class="card shadow p-3 mb-5 bg-white rounded">
+            <h1>&nbsp;&nbsp;Booking List</h1>
+            @foreach($pesanan as $p)
+            <div class="card shadow p-3 mb-5 bg-white rounded">
+                <div class="media position-relative ">
+                    <img src="{{asset('img/hotel/'.$p->gambarHotel1)}}" class="zoom mr-5 mt-4 ml-3 mb-3" alt="...">
+                    <div class="media-body mt-3 mb-3">
+                        <h5 class="detail-text font-weight-bold">Booking <i>#</i> {{$pesanan2[$loop->index]->id}}</h5>
+                        <h5 class="mt-3 detail-text">{{$p->namaHotel}}</h5>
+                        <p style="margin-bottom:0.25rem;">Price &emsp;&emsp;&emsp;&nbsp;&nbsp;: Rp. {{$p->hargaPesanan}} ,-</p>
+                        <p style="margin-bottom:0.25rem;">Check In &emsp;&emsp;: {{$p->checkIn}}</p>
+                        <p style="margin-bottom:0.25rem;">Check Out &emsp;&nbsp;: {{$p->checkOut}}</p>
+                        <p style="margin-bottom:0.25rem;">Book Time &emsp;: {{$p->jamBooking}}</p>
+                        <p style="margin-bottom:0.25rem;">Book Date &emsp;: {{$p->tanggalBooking}}</p>
+                        <a href="/invoice/{{$pesanan2[$loop->index]->id}}" class="stretched-link btn btn-primary mt-1">View Detail Booking</a>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
-        <!-- end of content -->
     </div>
+    <!-- end of content -->
     @include('Template/script')
 </body>
 
