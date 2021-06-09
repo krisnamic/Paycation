62ad37a619e3f5d6e874c9cac351b5bcdf86b9d5
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-06-09 21:34:09 +0700

    ombak footer dipindah ke template footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hotel</title>
+        <title>Paycation | Home</title>
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <link href="https://fonts.googleap0is.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -260,22 +260,7 @@
 
         <!-- Pantai -->
 
-        <div id="footer-transition">
-
-            <svg id="transition" viewBox="0 0 1440 232" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="-6" width="1452" height="232" fill="url(#paint0_linear)"/>
-                <defs>
-                    <linearGradient id="paint0_linear" x1="720" y1="0" x2="720" y2="232" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#FFFFFF"/>
-                        <stop offset="0.47708" stop-color="#FFFFFF"/>
-                        <stop offset="1" stop-color="white" stop-opacity="0"/>
-                    </linearGradient>
-                </defs>
-            </svg>
-
-            <img id="beach" src="{{asset('img/home/beach.svg')}}" alt="">
-
-        </div>
+        
 
         @include('Template/footer')
         
