8e79bd85f49be2c8ac06edd7c8dfd33f23a80038
steven278 <darkavenger347@gmail.com>
2021-06-10 01:30:44 +0700

    detailbooking improved

--- resources/views/User/detailBooking.blade.php
+++ resources/views/User/detailBooking.blade.php
@@ -21,21 +21,23 @@
 
     <!-- content -->
     <div class="container">
-        <div class="card shadow p-3 mb-5 bg-white rounded">
+        <div class="card shadow p-3 mb-5 bg-white rounded rounded-lg">
             <h1>&nbsp;&nbsp;Booking List</h1>
             @foreach($pesanan as $p)
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="media position-relative ">
-                    <img src="{{asset('img/hotel/'.$p->gambarHotel1)}}" class="zoom mr-5 mt-4 ml-3 mb-3" alt="...">
-                    <div class="media-body mt-3 mb-3">
+            <div class="card shadow p-3 mb-5 bg-white rounded rounded-lg">
+                <div class="media position-relative">
+                    <img src="{{asset('img/hotel/'.$p->gambarHotel1)}}" class="zoom mr-5 mt-4 ml-3 mb-3 rounded-lg" alt="...">
+                    <div class="media-body mt-4 mb-3">
                         <h5 class="detail-text font-weight-bold">Booking <i>#</i> {{$pesanan2[$loop->index]->id}}</h5>
-                        <h5 class="mt-3 detail-text">{{$p->namaHotel}}</h5>
+                        <h4 class="mt-3 detail-text font-weight-bold">{{$p->namaHotel}}</h4>
                         <p style="margin-bottom:0.25rem;">Price &emsp;&emsp;&emsp;&nbsp;&nbsp;: Rp. {{$p->hargaPesanan}} ,-</p>
                         <p style="margin-bottom:0.25rem;">Check In &emsp;&emsp;: {{$p->checkIn}}</p>
                         <p style="margin-bottom:0.25rem;">Check Out &emsp;&nbsp;: {{$p->checkOut}}</p>
                         <p style="margin-bottom:0.25rem;">Book Time &emsp;: {{$p->jamBooking}}</p>
                         <p style="margin-bottom:0.25rem;">Book Date &emsp;: {{$p->tanggalBooking}}</p>
-                        <a href="/invoice/{{$pesanan2[$loop->index]->id}}" class="stretched-link btn btn-primary mt-1">View Detail Booking</a>
+                        <div class="d-flex justify-content-end">
+                            <a href="/invoice/{{$pesanan2[$loop->index]->id}}" class="stretched-link btn btn-primary mt-1 ">View Detail Booking</a>
+                        </div>
                     </div>
                 </div>
             </div>
