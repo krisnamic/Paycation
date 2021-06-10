c98bffa8b353ff3f535134219b81466ea5f35642
steven278 <darkavenger347@gmail.com>
2021-06-10 09:39:39 +0700

    footer link & nim about us

--- resources/views/Template/footer.blade.php
+++ resources/views/Template/footer.blade.php
@@ -1,23 +1,23 @@
 <div id="footer-transition">
-    <svg id="transition" viewBox="0 0 1440 232" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="-6" width="1452" height="232" fill="url(#paint0_linear)"/>
-        <defs>
-            <linearGradient id="paint0_linear" x1="720" y1="0" x2="720" y2="232" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#FFFFFF"/>
-                <stop offset="0.47708" stop-color="#FFFFFF"/>
-                <stop offset="1" stop-color="white" stop-opacity="0"/>
-            </linearGradient>
-        </defs>
-    </svg>
+  <svg id="transition" viewBox="0 0 1440 232" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="-6" width="1452" height="232" fill="url(#paint0_linear)" />
+    <defs>
+      <linearGradient id="paint0_linear" x1="720" y1="0" x2="720" y2="232" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FFFFFF" />
+        <stop offset="0.47708" stop-color="#FFFFFF" />
+        <stop offset="1" stop-color="white" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+  </svg>
 
-    <img id="beach" src="{{asset('img/home/beach.svg')}}" alt="">
+  <img id="beach" src="{{asset('img/home/beach.svg')}}" alt="">
 
 </div>
 <!-- Footer -->
 <footer class="bg-dark text-center text-white">
   <!-- Copyright -->
   <div class="p-3 d-flex justify-content-between" style="background-color: #343A40;">
-    <p style="font-size: 1.2em;">© 2021 Copyright: <a href="sushipay-pemweb.xyz" style="font-size: 1.2em; color: cornflowerblue;">paycation.xyz</a> All rights reserved.</p>
+    <p style="font-size: 1.2em;">© 2021 Copyright: <a href="sushipay-pemweb.xyz" style="font-size: 1.2em; color: cornflowerblue;">hotel.sushipay-pemweb.xyz</a> All rights reserved.</p>
     <p style="font-size: 1.2em;">"You Sleep, You Pay, We Happy!"</p>
   </div>
   <!-- Copyright -->
